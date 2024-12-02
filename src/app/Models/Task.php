5899c0e9aa327c49fa1366979962b135e1cb06da
kazuma oshima <kxxxzg@gmail.com>
2024-12-03 07:47:40 +0900

    git ignoreの修正

--- src/app/Models/Task.php
+++ src/app/Models/Task.php
@@ -9,6 +9,13 @@ class Task extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'detail',
+        'deadline',
+        'status'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
